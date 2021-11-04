c304ed1896941e6249d93e02f24ec3a2b356f07f
phuockaus <phuoc.doan0412@hcmut.edu.vn>
2021-11-04 19:22:10 +0700

    Change name of class

--- Document.py
+++ Document.py
@@ -11,7 +11,7 @@ class Document(ABC):
             CONNECTION_STRING, collection_name)
 
     @staticmethod
-    def createDocuments(fileObject):
+    def __createDocuments(fileObject):
         item = {'Title': fileObject.getTitle(),
                 'Author': fileObject.getAuthor(),
                 'Content': fileObject.getContent(),
@@ -20,16 +20,17 @@ class Document(ABC):
         return item
 
     @classmethod
-    def pdf_push(cls, dbname, folder):
+    def __pdf_push(cls, dbname, folder):
         pdf_data = PDFReader.getPDFList(folder)
-        pdf_docs = list(map(lambda data: cls.createDocuments(data), pdf_data))
+        pdf_docs = list(
+            map(lambda data: cls.__createDocuments(data), pdf_data))
         if(pdf_docs != []):
             collection_name = dbname[folder]
             collection_name.insert_many(pdf_docs)
         PDFReader.clean(pdf_data)
 
     @classmethod
-    def docx_push(cls, dbname, folder):
+    def __docx_push(cls, dbname, folder):
         docx_data = DOCXReader.getDOCXList(folder)
         docx_docs = list(
             map(lambda data: cls.createDocuments(data), docx_data))
@@ -39,7 +40,7 @@ class Document(ABC):
         DOCXReader.clean(docx_data)
 
     def push(self, collection_eng, collection_vie):
-        self.pdf_push(self.dbname, collection_eng)
-        self.pdf_push(self.dbname, collection_vie)
-        self.docx_push(self.dbname, collection_eng)
-        self.docx_push(self.dbname, collection_vie)
+        self.__pdf_push(self.dbname, collection_eng)
+        self.__pdf_push(self.dbname, collection_vie)
+        self.__docx_push(self.dbname, collection_eng)
+        self.__docx_push(self.dbname, collection_vie)
