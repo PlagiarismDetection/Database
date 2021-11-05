3a7d0338a087a3b657b8cf389aafb02358c3bb62
phuockaus <phuoc.doan0412@hcmut.edu.vn>
2021-11-05 15:37:25 +0700

    Update methods

--- Document.py
+++ Document.py
@@ -14,29 +14,29 @@ class Document(ABC):
         return item
 
     @classmethod
-    def __pdf_push(cls, dbname, folder):
+    def __pdf_push(cls, dbname, folder, collection):
         pdf_data = PDFReader.getPDFList(folder)
         pdf_docs = list(
             map(lambda data: cls.__createDocuments(data), pdf_data))
         if(pdf_docs != []):
-            collection_name = dbname[folder]
+            collection_name = dbname[collection]
             collection_name.insert_many(pdf_docs)
         PDFReader.clean(pdf_data)
 
     @classmethod
-    def __docx_push(cls, dbname, folder):
+    def __docx_push(cls, dbname, folder, collection):
         docx_data = DOCXReader.getDOCXList(folder)
         docx_docs = list(
             map(lambda data: cls.__createDocuments(data), docx_data))
         if(docx_docs != []):
-            collection_name = dbname[folder]
+            collection_name = dbname[collection]
             collection_name.insert_many(docx_docs)
         DOCXReader.clean(docx_data)
 
     @classmethod
-    def push(cls, dbname, collection):
-        cls.__pdf_push(dbname, collection)
-        cls.__docx_push(dbname, collection)
+    def push(cls, dbname, folder, collection):
+        cls.__pdf_push(dbname, folder, collection)
+        cls.__docx_push(dbname, folder, collection)
 
     @staticmethod
     def getCollection(dbname, colname):
